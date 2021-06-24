8259777b0eb1e0886040eeaeb2b409088a150422
Kaymind <lempelziv@gmail.com>
2021-06-25 00:36:57 +0700

    refactor

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const inputEl = document.getElementById('input');
 const submitEl = document.getElementById('submit');
 const container = document.getElementById('container');
 
-let maxX = 0;
-let maxY = 0;
-let tempMaxX = 0;
-let tempMaxY = 0;
-let tracking = [];
-let resultTable;
-let tableSize;
-
 const createTable = (rows, columns, tracking, origin, tableSize) => {
   let array = new Array(rows);
 
@@ -40,40 +32,40 @@ const createTable = (rows, columns, tracking, origin, tableSize) => {
 const handleSubmit = (e) => {
   e.preventDefault();
   container.innerHTML = '';
-  maxX = 0;
-  maxY = 0;
-  tempMaxX = 0;
-  tempMaxY = 0;
-  tracking = [];
-  resultTable = '';
-  tableSize = 0;
+  let maxX = 0;
+  let maxY = 0;
+  let tempMaxX = 0;
+  let tempMaxY = 0;
+  let tracking = [];
+  let resultTable;
+  let tableSize;
 
   const inputArray = inputEl.value.split('');
 
   inputArray.forEach((ch) => {
     switch (ch.toLowerCase()) {
       case 'u':
-        maxX++;
-        if (maxX > tempMaxX) {
-          tempMaxX = maxX;
-        }
-        break;
-      case 'd':
-        maxX--;
-        if (Math.abs(maxX) > tempMaxX) {
-          tempMaxX = Math.abs(maxX);
-        }
-        break;
-      case 'r':
         maxY++;
         if (maxY > tempMaxY) {
           tempMaxY = maxY;
         }
         break;
-      case 'l':
+      case 'd':
         maxY--;
         if (Math.abs(maxY) > tempMaxY) {
           tempMaxY = Math.abs(maxY);
+        }
+        break;
+      case 'r':
+        maxX++;
+        if (maxX > tempMaxX) {
+          tempMaxX = maxX;
+        }
+        break;
+      case 'l':
+        maxX--;
+        if (Math.abs(maxX) > tempMaxX) {
+          tempMaxX = Math.abs(maxX);
         }
         break;
     }
