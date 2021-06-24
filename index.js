11336faf2794245ae99deab09c73f1e3f54e1e3f
Kaymind <lempelziv@gmail.com>
2021-06-25 00:40:38 +0700

    refactor

--- index.js
+++ index.js
@@ -3,30 +3,25 @@ const submitEl = document.getElementById('submit');
 const container = document.getElementById('container');
 
 const createTable = (rows, columns, tracking, origin, tableSize) => {
-  let array = new Array(rows);
-
   for (let i = 0; i < rows; i++) {
-    array[i] = new Array(columns);
     for (let j = 0; j < columns; j++) {
-      array[i][j] =
-        i === origin && j === origin
-          ? (container.innerHTML += `<div style='background-color:green; width: ${
-              100 / tableSize
-            }%; height: ${100 / tableSize}%' class='box'></div>`)
-          : i === tracking.slice(-1)[0][1] && j === tracking.slice(-1)[0][0]
-          ? (container.innerHTML += `<div style='background-color:red; width: ${
-              100 / tableSize
-            }%; height: ${100 / tableSize}%' class='box'></div>`)
-          : tracking.find((step) => step[1] === i && step[0] === j)
-          ? (container.innerHTML += `<div style='background-color:grey; width: ${
-              100 / tableSize
-            }%; height: ${100 / tableSize}%' class='box'></div>`)
-          : (container.innerHTML += `<div style='background-color:white; width: ${
-              100 / tableSize
-            }%; height: ${100 / tableSize}%' class='box'></div>`);
+      i === origin && j === origin
+        ? (container.innerHTML += `<div style='background-color:green; width: ${
+            100 / tableSize
+          }%; height: ${100 / tableSize}%' class='box'></div>`)
+        : i === tracking.slice(-1)[0][1] && j === tracking.slice(-1)[0][0]
+        ? (container.innerHTML += `<div style='background-color:red; width: ${
+            100 / tableSize
+          }%; height: ${100 / tableSize}%' class='box'></div>`)
+        : tracking.find((step) => step[1] === i && step[0] === j)
+        ? (container.innerHTML += `<div style='background-color:grey; width: ${
+            100 / tableSize
+          }%; height: ${100 / tableSize}%' class='box'></div>`)
+        : (container.innerHTML += `<div style='background-color:white; width: ${
+            100 / tableSize
+          }%; height: ${100 / tableSize}%' class='box'></div>`);
     }
   }
-  return array;
 };
 
 const handleSubmit = (e) => {
